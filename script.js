4ff63723d2cc8a3bfba529e66007b1ba89c0d425
NomadRebel <nikhilverma4209@gmail.com>
2025-04-21 13:01:38 +0530

    added a hover function so every div can change its color when you hover over it

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const main = document.querySelector("#main");
-const btn = document.querySelector("#create");
-btn.addEventListener("click", ()=>{
-  for(let i=0; i<33; i++){
-    const childDiv = document.createElement("div");
-    childDiv.textContent = `${i} it works`;
-    main.appendChild(childDiv);
-  }
-})
+for(let i=0; i<256; i++){
+  const childDiv = document.createElement("div");
+  childDiv.textContent = `${i}`;
+  childDiv.addEventListener("mouseover", ()=> {
+    childDiv.style.backgroundColor = "black";
+  })
+  childDiv.className = "child";
+  main.appendChild(childDiv);
+
+}
