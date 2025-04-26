ffa63926a97b5532fa6d64f4ea2c67cf124ad620
NomadRebel <nikhilverma4209@gmail.com>
2025-04-26 13:50:23 +0530

    succesfully took input from user and made a grid from it.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const main = document.querySelector("#main");
-for(let i=0; i<256; i++){
-  const childDiv = document.createElement("div");
-  childDiv.textContent = `${i}`;
-  childDiv.addEventListener("mouseover", ()=> {
-    childDiv.style.backgroundColor = "black";
-  })
-  childDiv.className = "child";
-  main.appendChild(childDiv);
+function sketch(){
+  const ask = window.prompt("Type your choice", '');
+  const size = (680/ask)-2;
+  const asks = ask*ask;
+  console.log(size);
+  for(let i=0; i<asks; i++){
 
+    const childDivOne = document.createElement("div");
+    childDivOne.style.width = size+"px";
+    childDivOne.style.height = size+"px";
+    childDivOne.addEventListener("mouseover", ()=> {
+    childDivOne.style.backgroundColor = "black";
+    })
+    childDivOne.className = "childOne";
+    main.appendChild(childDivOne);
+  }
 }
+
+const btn = document.querySelector("#start");
+btn.addEventListener("click",sketch);
